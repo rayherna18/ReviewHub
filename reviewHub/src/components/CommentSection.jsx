045279339a311b979e5311338722d955c4ab4bb1
Raymond Hernandez <raymondhr12@gmail.com>
2023-11-17 17:32:39 -0500

    Update CommentSection.jsx

--- reviewHub/src/components/CommentSection.jsx
+++ reviewHub/src/components/CommentSection.jsx
@@ -39,7 +39,7 @@ const CommentSection = ({ reviewId, userId }) => {
         } catch (error) {
             console.error("Error creating comment: ", error);
         }
-        window.location.href = `/reviews/${reviewId}`;
+        window.location.href = '/';
     };
 
     return (
